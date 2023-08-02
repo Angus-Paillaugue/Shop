2567d39f1c97525d44c22f0ce1c3dfa6b47d2b59
mere-patrie <angus.paillaugue40@gmail.com>
2023-08-02 18:50:11 +0200

    Added cookie modal, privacy-policy, about and stores section

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,62 @@
+<div class="w-full p-6 space-y-20 max-w-screen-2xl mx-auto">
+    <h5 class="mx-auto uppercase w-fit">About us</h5>
+
+    <div class="flex md:flex-row flex-col md:gap-20 gap-10">
+        <img src="https://nude-project.com/cdn/shop/files/image_5_1.png?v=1614766204" alt="" class="md:w-2/5 w-full">
+        <div class="flex flex-col justify-center">
+            <p class="leading-6">NUDE PROJECT is a ready-to-wear label founded in 2018 in a small dorm room by two friends eager to create something different. Bruno and Alex were raised in completely different environments. One from Bali, the other from Burgos (a little more north) had an instant connection that, after a month of knowing each other, led them to create a joint movement that would change their lives forever.</p>
+            <p class="leading-6">"For us, it was all about the hip-hop scene, the rawness of skateboarding and the artist’s creativity which motivated us to imprint a way of living shared through the internet."</p>
+            <h5 class="md:mt-20 mt-6">“By artists, for artists”</h5>
+        </div>
+    </div>
+
+    <div class="flex md:flex-row flex-col-reverse md:gap-20 gap-10">
+        <div class="flex flex-col justify-center md:gap-10 gap-4">
+            <div class="flex flex-row gap-10">
+                <p>BEHIND THE DESIGNS</p>
+                <p>We strive for designs that embrace a modern attitude for a creative life, seamlessly blending comfort, minimalism and elegance in every cut.</p>
+            </div>
+            <div class="md:px-6 px-10">
+                <img src="https://nude-project.com/cdn/shop/files/habussk_copy_6299e4b2-0de3-405c-9223-9a1d7e88543b.jpg?v=1615732958" alt="">
+            </div>
+        </div>
+        <img src="https://nude-project.com/cdn/shop/files/04-30_2.jpg?v=1615732721" alt="" class="md:w-2/5 w-full">
+    </div>
+
+    <div class="flex flex-col gap-10">
+        <div class="flex flex-row gap-20">
+            <p>A MOVEMENT</p>
+            <p>NUDE PROJECT is more than just clothing. It is a creative platform for the misfits, the outcasts and the wild to express themselves. Our garments aim to inspire the new generation to pursue their passion, orthodoxly or not, by empowering them to create without fear of judgement.</p>
+        </div>
+        <img src="https://nude-project.com/cdn/shop/files/image_6_3.png?v=1614767526" alt="w-10/12 mx-auto">
+    </div>
+
+    <div class="flex md:flex-row flex-col md:gap-20 gap-10">
+        <img src="https://nude-project.com/cdn/shop/files/6-2-4.jpg?v=1615733505" alt="" class="md:w-2/5 w-full">
+        <div class="flex flex-row gap-20 items-center md:pb-20">
+            <p>OUR PRINCIPLES</p>
+            <p>One of our goals is to change the perceived idea of success. We realised that focusing on money or fame just to seek acceptance, won't ever be fulfilling.</p>
+        </div>
+    </div>
+
+    <div class="flex md:flex-row flex-col-reverse md:gap-20 gap-10">
+        <div class="flex flex-row gap-10 items-center md:pb-20">
+            <p>VIRALITY</p>
+            <p>Our first big statement was when we gained over 60,000 followers overnight. Some people called it luck, but if there is something we are certain about is that the harder we work, the more luck we have.</p>
+        </div>
+        <img src="https://nude-project.com/cdn/shop/files/0147c27f-3c3e-4b59-85db-89d2586f3b52.jpg?v=1615734115" alt="" class="md:w-2/5 w-full">
+    </div>
+
+    <div class="flex md:flex-row flex-col md:gap-20 gap-10">
+        <img src="https://nude-project.com/cdn/shop/files/04-33_cdde09e7-aac0-440c-a605-9b2a4dad1e58.jpg?v=1615734313" alt="" class="md:w-2/5 w-full">
+        <div class="flex flex-row gap-20 items-center md:pb-20">
+            <div class="flex flex-col w-full items-center">
+                <p>VISUALS + SOUNDS</p>
+                <a href="https://open.spotify.com/user/brunocasanovas21?si=9ETT0vlUTly_FIR-mVDScA"><img src="https://nude-project.com/cdn/shop/files/image_7_1.png?v=1614777129" alt="" class="w-10 h-10 mt-4"></a>
+            </div>
+            <p>More than a brand, we have become a lifestyle, with music that narrates our journey. Hip hop, urban latin and a subtle touch of diverse genres. All good music. Here’s the link to our Spotify playlist, the official soundtrack to the movement.</p>
+        </div>
+    </div>
+
+    <p class="mx-5">Early on we realised the traditional path did not fit us: go to university, get the job that your parents told you to, pay your mortgage… Following our principles we itched to start working. Because you are the only person stopping yourself from doing what you love.</p>
+</div>
---
+++ src/routes/stores/+page.svelte
@@ -0,0 +1,138 @@
+<script>
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    import '@splidejs/svelte-splide/css';
+</script>
+
+<div class="w-full p-6 space-y-20 max-w-screen-2xl mx-auto">
+
+    <div class="flex lg:flex-row flex-col gap-4">
+        <h1 class="max-lg:text-center">
+            <sup>Our </sup>
+            <span> Stores</span>
+        </h1>
+        <div class="flex flex-col gap-2 justify-end">
+            <p class="font-bold text-xl lg:text-start text-center">We already are in Madrid, Valencia and Barcelona</p>
+            <p class="text-base lg:text-start text-center">Nude Project is a ready-to-wear label founded in 2018 in a small dorm room by two friends eager to create something different. Now we have a real place were capture the essence of the brand made for the Nude Cult.</p>
+        </div>
+    </div>
+
+    <div class="flex lg:flex-row flex-col gap-10 justify-between">
+        <div class="flex flex-col">
+            <a href="https://goo.gl/maps/nDCsRe9WFoJQqUmE6" class="link uppercase font-extrabold">Barcelona</a>
+            <p class="text-xs mt-4">De la Canuda Street, 37</p>
+            <p class="text-xs">08002 Barcelona</p>
+            <div class="flex flex-col mt-4">
+                <div class="flex flex-row justify-between md:w-44"><p>MON-SAT</p><p>11-21</p></div>
+                <div class="flex flex-row justify-between md:w-44"><p>SUN</p><p>12-20</p></div>
+            </div>
+        </div>
+
+        <Splide options={ { rewind: true, gap:"1rem", height:"35rem" } } class="lg:w-4/5">
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_51_454eb3d4-95ee-4963-947e-5f7fc6c7ac89.jpg?v=1681897616" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_89_aa60f7be-015b-4a73-b0e7-5e470f5cb426.jpg?v=1687043051" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_62_03db84e1-1efb-4bc8-ad5b-7a8cf5f8a113.jpg?v=1686310685" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_82_8d10d50c-fadc-41ce-9de6-9eee6107aab2.jpg?v=1681897506" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_66_079d8a78-1786-45d5-8d09-a23804d8cb4c.jpg?v=1687042969" alt="">
+            </SplideSlide>
+        </Splide>
+    </div>
+
+    <div class="flex lg:flex-row flex-col gap-10 justify-between">
+        <div class="flex flex-col">
+            <a href="https://goo.gl/maps/nVHytFeWjQ6CejE49" class="link uppercase font-extrabold">Valencia</a>
+            <p class="text-xs mt-4">Martínez Ferrando Street, 1</p>
+            <p class="text-xs">46004 Valencia</p>
+            <div class="flex flex-col mt-4">
+                <div class="flex flex-row justify-between md:w-44"><p>MON-SAT</p><p>11-21</p></div>
+                <div class="flex flex-row justify-between md:w-44"><p>SUN</p><p>closed</p></div>
+            </div>
+        </div>
+
+        <Splide options={ { rewind: true, gap:"1rem", height:"35rem" } } class="lg:w-4/5">
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_74_3d432bc0-8477-470c-924c-fb5a86f7ec5c.jpg?v=1687043166" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_69_ace3dd0b-90ea-4dff-ba2e-633590f3e140.jpg?v=1681460196" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_70_fce1fe51-a35c-4f55-a365-19950e36340b.jpg?v=1681460390" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/2_90f4012b-dd1d-48c5-b75d-eb09d8f05bf5.jpg?v=1681894285" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_80_3f8ed425-db7c-4960-89fa-16e2fba07fd4.jpg?v=1687043188" alt="">
+            </SplideSlide>
+        </Splide>
+    </div>
+
+    <div class="flex lg:flex-row flex-col gap-10 justify-between">
+        <div class="flex flex-col">
+            <a href="https://goo.gl/maps/4VWyN5MvFrjTzKD98" class="link uppercase font-extrabold">Madrid</a>
+            <p class="text-xs mt-4">Fuencarral Street, 47</p>
+            <p class="text-xs">28004 Madrid</p>
+            <div class="flex flex-col mt-4">
+                <div class="flex flex-row justify-between md:w-44"><p>MON-SAT</p><p>11-21</p></div>
+                <div class="flex flex-row justify-between md:w-44"><p>SAT</p><p>11-21.30</p></div>
+                <div class="flex flex-row justify-between md:w-44"><p>SUN</p><p>12-20</p></div>
+            </div>
+        </div>
+
+        <Splide options={ { rewind: true, gap:"1rem", height:"35rem" } } class="lg:w-4/5">
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_96.jpg?v=1681899623" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_97.jpg?v=1681899624" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_100.jpg?v=1681899624" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_99.jpg?v=1681899624" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_103.jpg?v=1681899624" alt="">
+            </SplideSlide>
+        </Splide>
+    </div>
+
+    <div class="flex lg:flex-row flex-col gap-10 justify-between">
+        <div class="flex flex-col">
+            <a href="https://goo.gl/maps/FWTSDqcReyZq4bPr6" class="link uppercase font-extrabold">La roca village</a>
+            <p class="text-xs mt-4">La Roca Village, Barcelona</p>
+            <p class="text-xs">08430 Santa Agnès de Malanyanes</p>
+            <div class="flex flex-col mt-4">
+                <div class="flex flex-row justify-between md:w-44"><p>MON-SUN</p><p>11-22</p></div>
+            </div>
+        </div>
+
+        <Splide options={ { rewind: true, gap:"1rem" } } class="lg:w-4/5">
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/1_1f28789a-94dc-4775-bbd7-e089abccfe1d.jpg?v=1690808571" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/2_71fb5dfa-8e7f-4c27-9329-3a247f179ac2.jpg?v=1690808585" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/4_7190e88c-921d-402f-a45e-91ac367e12bb.jpg?v=1690808585" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/iPhone_14_Pro_Max_99.jpg?v=1681899624" alt="">
+            </SplideSlide>
+            <SplideSlide class="flex items-center justify-center">
+                <img src="https://nude-project.com/cdn/shop/files/3_2f062b09-d131-49cf-a708-d0b2b3f7bace.jpg?v=1690808585" alt="">
+            </SplideSlide>
+        </Splide>
+    </div>
+</div>

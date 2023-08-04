151bdf7c72dd08aff615d4140e4bc91d5730e06a
mere-patrie <angus.paillaugue40@gmail.com>
2023-08-04 17:27:54 +0200

    Error handling and error page building

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,14 @@
 <script>
     import { page } from '$app/stores';
+    import { pageMetaData } from "$lib/stores";
+
+    $pageMetaData.title = $page.status || "404" + "Error";
 </script>
 
-<svelte:head>
-	<title>{$page.status || "404"} Error</title>
-</svelte:head>
 
-<div class="hero min-h-screen">
-    <div class="hero-content flex-col">
-        <h1 class="mb-6 text-7xl tracking-tight font-extrabold lg:text-9xl">{$page.status || "404"}</h1>
-        <p class="mb-6 text-2xl tracking-tight font-bold">{$page.error.message || "Page not found"}</p> 
-    
-        <a href="{$page.error.link || "/"}" class="btn btn-primary">{$page.error.buttonText || "Back to Homepage"}</a>
-    </div>
+<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center flex-1">
+    <h1 class="mb-6 text-7xl tracking-tight font-extrabold lg:text-9xl">{$page.status || "404"}</h1>
+    <p class="mb-6 text-xl tracking-tight font-bold">{$page.error.message || "Page not found"}</p> 
+
+    <a href="{$page.error.link || "/"}" class="button-primary">{$page.error.buttonText || "Back to Homepage"}</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,6 @@
     $pageMetaData.description = "Nude Project is a ready-to-wear label founded in 2018 in a small dorm room by two friends eager to create something different. Our garments aim to inspire the new generation to pursue their passion, whether or not it is the traditional path, by empowering artists to create without fear of judgement.";
 </script>
 
-<section></section>
 <section class="p-6 justify-between w-full">
     <h6 class="font-semibold uppercase text-sm mb-6">New arrivals</h6>
     <div class="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-6">
